a91f398417cfb3e67fa785b758bdc3e63c3786d9
Shaun Kyle <shaun@jpk.co.nz>
2025-04-01 11:30:09 +1100

    fix import path (renamed module from src to dsail)

--- dsail/__init__.py
+++ dsail/__init__.py
@@ -1,5 +1,5 @@
 # All exports required to recreate run_12ECG_classifier.py
-import src.config as config
-from src.data import get_dataset_from_configs, collate_into_list, get_loss_weights_and_flags
-from src.model.model_utils import get_model
-from src.train import Trainer
+import dsail.config as config
+from dsail.data import get_dataset_from_configs, collate_into_list, get_loss_weights_and_flags
+from dsail.model.model_utils import get_model
+from dsail.train import Trainer
