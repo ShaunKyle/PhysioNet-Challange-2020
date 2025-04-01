270c965b4cfe64a04d8e73d5d779a3974d362a67
Shaun Kyle <shaun@jpk.co.nz>
2025-04-01 11:42:10 +1100

    remove unnecessary exports

--- dsail/__init__.py
+++
@@ -1,5 +0,0 @@
-# All exports required to recreate run_12ECG_classifier.py
-import dsail.config as config
-from dsail.data import get_dataset_from_configs, collate_into_list, get_loss_weights_and_flags
-from dsail.model.model_utils import get_model
-from dsail.train import Trainer
